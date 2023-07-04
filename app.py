5e67ea2690a161f8215fba9b6285b5735f17ba51
Dnzl.exe <cempronziel55@gmail.com>
2023-07-04 10:59:50 +0800

    updated requirements

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 """
 Adding cartwheeling (🤸‍♀️) emojis between spaces
 """
+# Importing clipboard
+import clipboard as cp
+
 # Importing streamlit
 import streamlit as st
 
 # Setting page Title and Icon
 st.set_page_config(page_title="Beshy App", page_icon="🤸‍♀️")
 
-st.text_area("Bakit tinatamad mag-type ang beshy ko?")
+# Adding title
+st.title("🤸‍♀️🤸‍♀️🤸‍♀️")
+
+# Adding text input 
+st.text_area(label=" ",placeholder="Bakit tinatamad mag-type ang beshy ko?")
+
+# Adding text output
+
+
+# Adding copy button
+
