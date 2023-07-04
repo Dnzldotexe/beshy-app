3e0c32fdd99653c8c7f699f244f6bbfaad642c29
Dnzl.exe <cempronziel55@gmail.com>
2023-07-04 10:32:49 +0800

    coded app

---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+Adding cartwheeling (🤸‍♀️) emojis between spaces
+"""
+# Importing streamlit
+import streamlit as st
+
+# Setting page Title and Icon
+st.set_page_config(page_title="Beshy App", page_icon="🤸‍♀️")
+
+st.text_area("Bakit tinatamad mag-type ang beshy ko?")
